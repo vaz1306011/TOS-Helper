9e5f7c657748df7d9fc00ccb0620d72056b393dc
小飛 <vaz1306011@gmail.com>
2025-09-17 14:36:20 +0900

    Initial Commit

---
+++ TOS Helper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TOS Helper
+//
+//  Created by sora on 2025/9/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TOS Helper/TOS_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOS_HelperApp.swift
+//  TOS Helper
+//
+//  Created by sora on 2025/9/17.
+//
+
+import SwiftUI
+
+@main
+struct TOS_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
