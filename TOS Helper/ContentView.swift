91144313fc40aabfccc3b2f94a176ba473edb052
小飛 <vaz1306011@gmail.com>
2025-10-07 02:05:43 +0900

    format toolbar, sheet, alert

--- TOS Helper/ContentView.swift
+++ TOS Helper/ContentView.swift
@@ -11,18 +11,20 @@ struct ContentView: View {
   // MARK: - Properties
   @State private var tabs: [GameData] = [.init(name: "New Game", recoveryInterval: 8)]
 
-  @State private var showAddingTab: Bool = false
-  @State private var showDeleteAlert : Bool = false
-  
+  @State private var showAddTab: Bool = false
+  @State private var showEditTab: Bool = false
+  @State private var showDeleteAlert: Bool = false
+
   @State private var selection: UUID? = nil
-  @State private var tempTab: GameData?
-  private var currentTabTitle: String {
+  @State private var editingTab: GameData?
+  @State private var deletingTab: GameData?
+  private var currentTab: GameData? {
     if let selection,
        let tab = tabs.first(where: { $0.id == selection })
     {
-      return tab.name
+      return tab
     }
-    return "nil Title"
+    return nil
   }
 
   // MARK: - Body
@@ -35,6 +37,7 @@ struct ContentView: View {
               Image(systemName: "square.fill")
               Text(tab.name.wrappedValue)
             }
+            .id(tab.id)
             .tag(tab.id)
         }
       }
@@ -43,63 +46,97 @@ struct ContentView: View {
           selection = tabs.first?.id
         }
       }
-      .navigationTitle(currentTabTitle)
-      .toolbar {
-        ToolbarItem(placement: .navigationBarTrailing) {
-          Menu {
-            Button("add") { addTab() }
-            Button("edit") { editCurrentTab() }
-            Button("delete") { deleteCurrentTab() }
-          } label: {
-            Image(systemName: "ellipsis.circle.fill")
-              .font(.title)
-          }
-        }
-      }
-      .sheet(isPresented: $showAddingTab, content: {
-        AddTabView(
-          onSave: { tab in
-            tabs.append(tab)
-            showAddingTab = false
-            selection = tab.id
-          }, onCancel: {
-            showAddingTab = false
-          }
-        )
-      })
-      .sheet(item: $tempTab) { tab in
-        if let index = tabs.firstIndex(where: { $0.id == tab.id }) {
-          EditTabView(
-            gameData: $tabs[index],
-            onSave: { tempTab = nil },
-            onCancel: { tempTab = nil }
-          )
-        }
+      .navigationTitle(currentTab?.name ?? "nil title")
+      .toolbar { toolBarButtons }
+      .sheet(isPresented: $showAddTab) { addTabSheet }
+      .sheet(isPresented: $showEditTab) { editTabSheet }
+      .overlay { deleteAlert }
+      .id(UUID())
+    }
+  }
+}
+
+// MARK: - ToolBarItem
+private extension ContentView {
+  @ToolbarContentBuilder
+  var toolBarButtons: some ToolbarContent {
+    ToolbarItem(placement: .navigationBarTrailing) {
+      Menu {
+        Button("add") { addTab() }
+        Button("edit") { editCurrentTab() }
+        Button("delete") { deleteCurrentTab() }
+      } label: {
+        Image(systemName: "ellipsis.circle.fill")
+          .font(.title)
       }
     }
+  }
+}
+
+// MARK: - Sheet and Alert
+private extension ContentView {
+  @ViewBuilder
+  var addTabSheet: some View {
+    AddTabView(
+      onSave: { tab in
+        tabs.append(tab)
+        showAddTab = false
+        selection = tab.id
+      }, onCancel: {
+        showAddTab = false
+      }
+    )
+  }
+
+  @ViewBuilder
+  var editTabSheet: some View {
+    if let index = tabs.firstIndex(where: { $0.id == currentTab?.id }) {
+      EditTabView(
+        gameData: $tabs[index],
+        onSave: { showEditTab = false },
+        onCancel: { showEditTab = false }
+      )
+    }
+  }
+
+  @ViewBuilder
+  var deleteAlert: some View {
+    EmptyView()
+      .alert(
+        "confirm_delete",
+        isPresented: $showDeleteAlert
+      ) {
+        Button("delete", role: .destructive) {
+          if let index = tabs.firstIndex(where: { $0.id == currentTab?.id }) {
+            let safeIndex = min(index + 1, tabs.count - 1)
+            selection = tabs[safeIndex].id
+            tabs.remove(at: index)
+          }
+        }
+        Button("cancel", role: .cancel) {}
+      } message: {
+        Text(String(
+          format: NSLocalizedString("confirm_delete_message", comment: ""),
+          currentTab?.name ?? ""
+        ))
+      }
   }
 }
 
 // MARK: - Menu Logic
 private extension ContentView {
   func addTab() {
-    showAddingTab = true
+    showAddTab = true
   }
 
   private func editCurrentTab() {
-    guard let currentId = selection,
-          let tab = tabs.first(where: { $0.id == currentId }) else { return }
-    tempTab = tab
+    editingTab = currentTab
+    showEditTab = true
   }
 
   private func deleteCurrentTab() {
-    guard let currentId = selection,
-          let index = tabs.firstIndex(where: { $0.id == currentId }) else { return }
-    tabs.remove(at: index)
-    if tabs.isEmpty {
-      addTab()
-    }
-    selection = tabs.first?.id
+    deletingTab = currentTab
+    showDeleteAlert = true
   }
 }
 
